99b0023c9554aa480565ac30413cad521612d35a
Mahmudul107 <mahmudulislam378@gmail.com>
2023-02-18 10:34:33 +0600

    triagle area almost done

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// First card
+document.getElementById('btn-triangle').addEventListener('click', function(){
+    const hFieldString = document.getElementById('h-field').value;
+    const hField = parseFloat(hFieldString);
+    
+    const bFieldString = document.getElementById('b-field').value;
+    const bField = parseFloat(bFieldString);
+    
+    const TriangleArea = document.getElementById('area-triangle').innerText;
+    
+
+// Create Table row 
+})
