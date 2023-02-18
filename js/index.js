59d2cdd2a3ed7a2235ed1b14c685007e1224bc74
Mahmudul107 <mahmudulislam378@gmail.com>
2023-02-18 12:35:46 +0600

    triangle card multiplication complete

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
-// First card
+// First card: Getting data from html file
 document.getElementById('btn-triangle').addEventListener('click', function(){
     const hFieldString = document.getElementById('h-field').value;
     const hField = parseFloat(hFieldString);
     
     const bFieldString = document.getElementById('b-field').value;
     const bField = parseFloat(bFieldString);
-    
-    const TriangleArea = document.getElementById('area-triangle').innerText;
+
+    // hField = '';
+    // bField = '';
+    const totalArea = (0.05 * hField * bField).toFixed(2);
+    // const triangleAreaField = document.getElementById('area-triangle').innerText;
+    // triangleAreaField = totalArea;
     
 
-// Create Table row 
+
+// Create Table row
+    const tableContainer = document.getElementById('table-container');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>Triangle</td>
+    <td>${totalArea} cm&sup2;</td>
+    <td> <button>Convert to m&sup2;</button></td>
+   
+    `
+    tableContainer.appendChild(tr);
 })
