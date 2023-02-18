69a8bd3b3b5f2d18c557191416e7593bcfec233b
Mahmudul107 <mahmudulislam378@gmail.com>
2023-02-18 13:43:58 +0600

    Triagle area validation complete

--- js/index.js
+++ js/index.js
@@ -1,20 +1,6 @@
-// First card: Getting data from html file
-document.getElementById('btn-triangle').addEventListener('click', function(){
-    const hFieldString = document.getElementById('h-field').value;
-    const hField = parseFloat(hFieldString);
-    
-    const bFieldString = document.getElementById('b-field').value;
-    const bField = parseFloat(bFieldString);
-
-    // hField = '';
-    // bField = '';
-    const totalArea = (0.05 * hField * bField).toFixed(2);
-    // const triangleAreaField = document.getElementById('area-triangle').innerText;
-    // triangleAreaField = totalArea;
-    
-
-
-// Create Table row
+// Common function for area calculation 
+function areaCalculation(hField, bField, totalArea) {
+    // console.log('called')
     const tableContainer = document.getElementById('table-container');
 
     const tr = document.createElement('tr');
@@ -26,4 +12,24 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
    
     `
     tableContainer.appendChild(tr);
+}
+
+// First card: Getting data from html file
+document.getElementById('btn-triangle').addEventListener('click', function () {
+    const hFieldString = document.getElementById('h-field').value;
+    const bFieldString = document.getElementById('b-field').value;
+
+//Input values are valid numbers or not
+    if (isNaN(hFieldString) || isNaN(bFieldString)) {
+        alert('Please enter valid numbers.');
+        return;
+    }
+
+
+    const bField = parseFloat(bFieldString);
+    const hField = parseFloat(hFieldString);
+    const totalArea = (0.05 * hField * bField).toFixed(2);
+
+
+    // Create Table row
 })
